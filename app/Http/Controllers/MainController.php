e678d629a6a9878b79ad43e55d0770a47605c599
Luciano Eduardo <luciano123ab@gmail.com>
2026-01-10 20:09:07 -0300

    003(QueriesQueryBuilder_Parte1)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,10 +9,41 @@ class MainController extends Controller
 {
     public function index() {
 
-        $clients = DB::table("clients")->get();
+        //Devolver todos os dados de uma tabela:
+        // $clients = DB::table("clients")->get();
 
-        //$this->showRawTable($clients);
-        $this->showDataTable($clients);
+        //Apresentar num array associativo:
+        // $clients = DB::table("clients")->get()->toArray();
+
+        //Apresentar num array de arrays associativos:
+        // $results = DB::table("products")->get()->map(function($item) {
+        //     return (array) $item;
+        // });
+
+        //Apresentar os dados a partir dos resultados:
+        // $products = DB::table("products")->get();
+
+        // foreach ($products as $product) {
+        //     echo $product->product_name . "<br>";
+        // }
+
+        //Apresentar apenas algumas colunas:
+        // $products = DB::table("products")->get(["product_name", "price"]);
+
+        //pluck - Obter de forma simples os dados de uma coluna específica:
+        // $results = DB::table("products")->pluck("product_name");
+
+        //Devolver apenas a primeira linha de um resultado:
+        // $results = DB::table("products")->get()->first();
+
+        //Devolver apenas a última linha de um resultado:
+        // $results = DB::table("products")->get()->last();
+
+        //SELECT * FROM products WHERE id = 10:
+        $results = DB::table("products")->find(10);
+
+        $this->showRawTable($results);
+        // $this->showDataTable($results);
     }
 
     private function showRawTable($data) {
