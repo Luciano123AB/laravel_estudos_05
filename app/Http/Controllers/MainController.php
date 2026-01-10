af520514aced824f60ad8c75143fdfe9c45f66a5
Luciano Eduardo <luciano123ab@gmail.com>
2026-01-10 19:51:04 -0300

    002(ComoFunciona_QueryBuilder)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,8 +8,35 @@ use Illuminate\Support\Facades\DB;
 class MainController extends Controller
 {
     public function index() {
-        DB::connection()->getPdo();
 
-        echo "OK!";
+        $clients = DB::table("clients")->get();
+
+        //$this->showRawTable($clients);
+        $this->showDataTable($clients);
+    }
+
+    private function showRawTable($data) {
+        echo "<pre>";
+            print_r($data);
+        echo "</pre>";
+    }
+
+    private function showDataTable($data) {
+        echo "<table border='1'>";
+            //Header:
+            echo "<tr>";
+                foreach ($data[0] as $key => $value) {
+                    echo "<th>" . $key . "</th>";
+                }
+            echo "</tr>";
+
+            foreach ($data as $row) {
+                echo "<tr>";
+                    foreach ($row as $key => $value) {
+                        echo "<td>" . $value . "</td>";
+                    }
+                echo "</tr>";
+            }
+        echo "</table";
     }
 }
